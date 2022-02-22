f0e7b7e711b3f38ccfb9185bab6e79f236176516
Sandro <gandalfr271@gmail.com>
2022-02-22 15:47:17 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingApp
+{
+    class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int courses;
+            courses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite book: ");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled courses: " + courses);
+            Console.WriteLine("Price of my favorite book: " + price);
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
