fd3bcb0a2789d97bf845960296d2985cda2c7559
Sandro <gandalfr271@gmail.com>
2022-04-04 23:04:52 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class Form1 : Form
+    {
+        double num1;
+        double num2;
+        double num3;
+        double num4;
+        double num5;
+        String name;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+             num1 = double.Parse(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+             num2 = double.Parse(textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+             num3 = double.Parse(textBox4.Text);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+             num4 = double.Parse(textBox5.Text);
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+             num5 = double.Parse(textBox6.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sum = num1 + num2 + num3 + num4 + num5;
+            double average = sum / 5;
+            if (average >= 75)
+            {
+                label8.Text = "The student passed.";
+                label9.Text = "The general average of " + name + " is " + average.ToString("#,0.00")+".";
+            }
+            else
+                label8.Text = "The student failed.";
+                label9.Text = "The general average of " + name + " is " + average.ToString("#,0.00")+ ".";
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+        }
+    }
+}
---
+++ frmStudentRegistration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistrationApplication
+{
+    public partial class frmStudentRegistration : Form
+    {
+        public frmStudentRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 31; i++)
+            {
+                Day_combobx.Items.Add(i.ToString());
+            }
+
+            for(int i = 1; i <= 12; i++)
+            {
+                Month_combobx.Items.Add(i.ToString());
+            }
+
+           for(int i = 1900; i <= 2022; i++)
+            {
+                Year_combobx.Items.Add(i.ToString());
+            }
+        }
+
+
+        private void Day_combobx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Reg_button_Click(object sender, EventArgs e)
+        {
+            String FirstName = FirstName_txtbox.Text.ToString();
+            String LastName = LastName_txtbox.Text.ToString();
+            String MiddleName = MiddleName_txtbox.Text.ToString();
+            String BirthDay = Day_combobx.Text.ToString();
+            String BirthMonth = Month_combobx.Text.ToString();
+            String BirthYear = Year_combobx.Text.ToString();
+
+            if (male_radiobtn.Checked)
+            {
+                MessageBox.Show("Student Name: " + FirstName + " " + MiddleName + " " + LastName +
+                    "\nGender: Male" +
+                    "\nDate of Birth: " + BirthDay + "/" + BirthMonth + "/" + BirthYear);
+            }
+            else if(female_radiobtn.Checked)
+            {
+                MessageBox.Show("Student Name: " + FirstName + " " + MiddleName + " " + LastName +
+                   "\nGender: Female" +
+                   "\nDate of Birth: " + BirthDay + "/" + BirthMonth + "/" + BirthYear);
+            }
+            
+        }
+
+        private void male_radiobtn_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void female_radiobtn_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
