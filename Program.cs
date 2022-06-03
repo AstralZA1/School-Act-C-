39302af0e2ba69be9fc2969dff434f74a3dca3cb
Sandro <gandalfr271@gmail.com>
2022-06-03 10:23:07 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,22 @@
 ﻿using System;
-using System.Collections;
-using System.Text;
 
-namespace GuessingGameTP
+namespace Circle
 {
-    class Guess
-    {
-        ArrayList listofwords = new ArrayList { "moon", "star", "hello", "world", "dog", "cat", "astral", "selene", "jury", "night" };
-        Random number = new Random();
-        int tries = 0;
-        bool guess = true;
-        public void game()
-        {
-            int random = number.Next(0, 9);
-            String word = (string)listofwords[random];
-            StringBuilder wordtoguess = new StringBuilder(String.Empty.PadLeft(word.Length, '*'));
-            Console.WriteLine("Welcome to Word Guessing Game!");
-            Console.WriteLine("You have a maximum of 10 tries to guess the word");
-            Console.WriteLine("Word to Guess is: " + wordtoguess);
-            while (guess == true)
-            {
-                Console.Write("\nEnter a letter: ");
-                tries++;
-                char letter = Convert.ToChar(Console.ReadLine());
-                letter = Char.ToLower(letter);
-                for (int i = 0; i < word.Length; i++)
-                {
-                    if (letter == word[i])
-                    {
-                        wordtoguess[i] = letter;
-                    }
-                }
-                Console.WriteLine(wordtoguess.ToString());
-                if (wordtoguess.ToString() == word)
-                {
-                    Console.WriteLine("Congratulations! You win!" + "\nThe word is: " + word);
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Keep Guessing");
-                    if (tries == 10)
-                    {
-                        Console.WriteLine("You have already guessed 10 times, you failed to guess the word :( " + "\nThe word is: " + word);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    class Program
+    class CircleDiameter
     {
         static void Main(string[] args)
         {
-            Guess play = new Guess();
-            play.game();
+            CircleDiameter obj = new CircleDiameter();
+            Console.Write("Enter a radius: ");
+            double rad;
+            rad = obj.getCircleDiamter(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("The diameter is: " + rad);
+        }
+        private double getCircleDiamter(double radius)
+        {
+            double diameter = radius * 2;
+            return diameter;
+        }
         }
     }
-}
+
